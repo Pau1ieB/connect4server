03346c6957236d4e884dbf958e30a14d6aee0fa2
Pau1ieB <paul56757@aol.com>
2023-11-03 11:11:57 +0000

    another version

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
-const listOfGames = require('./games/listOfGames');
+let listOfGames = require('./games/listOfGames');
 let listOfMessages=require('./games/messages');
 let gameIndex=0;
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next)=>{
+    console.log(req.url)
+    next();
+});
 
 app.get('/gameslist',(req,res)=>{
-    const games = listOfGames.filter(game=>!game.active);
-    res.send(games)
+    res.send(listOfGames)
 })
 
-app.get('/message',(req,res)=>{
-    const name = req.query.name;
-    const id = parseInt(req.query.id);
+app.get('/message/:name/:id',(req,res)=>{
+    const name = req.params.name;
+    const id = parseInt(req.params.id);
     const messages = listOfMessages.filter(message=>message.name!==name && message.id==id);
     listOfMessages = listOfMessages.filter(message=>(message.name===name && message.id==id) || message.id!=id);
     res.status(200).send(messages)
@@ -27,14 +30,13 @@ app.get('/allmessages',(req,res)=>{
     res.status(200).send(listOfMessages)
 })
 
-app.post('/creategame',(req,res)=>{
+app.post('/host',(req,res)=>{
     if(!req.body.name){
         res.status(404)
     }
     else{
         listOfGames.push({
             name:req.body.name,
-            active:false,
             id:gameIndex
         });
         res.status(201).send({message:"Game created. Waiting for an opposition player.",id:gameIndex++});
@@ -44,17 +46,16 @@ app.post('/creategame',(req,res)=>{
 app.put('/joingame',(req,res)=>{
     const id = parseInt(req.body.id);
     const game = listOfGames.find(game=>game.id==id);
-    game.active=true;
+    listOfGames = listOfGames.filter(game=>game.id!=id);
     listOfMessages.push({
         name:"oppo",
         id:id,
         message:"You have an opponent."
-    });        
+    });
     res.status(200).send({});
-
 })
 
-app.post('/postmessage',(req,res)=>{
+app.post('/sendmessage',(req,res)=>{
     listOfMessages.push(req.body);
     res.status(200).send({});
 })
